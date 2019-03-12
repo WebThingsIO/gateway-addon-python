3f0393ea83013d7516f32d279f2e72d8d1a6c0f3
Michael Stegeman <michael@stegeman.me>
2019-03-12 09:10:01 -0600

    Bump dependencies. (#35)

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+import sys
 
 
 here = path.abspath(path.dirname(__file__))
@@ -10,6 +11,18 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file.
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+requirements = [
+    'nnpy==1.4.2',
+]
+
+if sys.version_info.major == 2:
+    requirements.append('jsonschema==2.6.*')
+elif sys.version_info.major == 3:
+    if sys.version_info.minor < 5:
+        requirements.append('jsonschema==2.6.*')
+    else:
+        requirements.append('jsonschema>=3.0.0')
 
 setup(
     name='gateway_addon',
@@ -20,5 +33,5 @@ setup(
     author='Mozilla IoT',
     keywords='mozilla iot gateway addon add-on',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=['jsonschema', 'nnpy'],
+    install_requires=requirements,
 )
