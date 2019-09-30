58a2b7990fb34545c452d310f11c6fcfba66da46
Michael Stegeman <michael@stegeman.me>
2019-09-30 10:21:30 -0800

    Validate incoming messages via schema.

--- gateway_addon/ipc.py
+++ gateway_addon/ipc.py
@@ -4,14 +4,47 @@ from __future__ import print_function
 from nnpy.errors import NNError
 import functools
 import json
+import jsonschema
 import nnpy
+import os
 
 from .constants import MessageType
 
 
 _IPC_BASE = 'ipc:///tmp/'
+_SCHEMA_DIR = os.path.realpath(
+    os.path.join(os.path.dirname(__file__), 'schema')
+)
 
 print = functools.partial(print, flush=True)
+
+
+class Resolver(jsonschema.RefResolver):
+    """Resolver for $ref members in schemas."""
+
+    def __init__(self):
+        """Initialize the resolver."""
+        jsonschema.RefResolver.__init__(
+            self,
+            base_uri='',
+            referrer=None,
+            cache_remote=True,
+        )
+
+    def resolve_remote(self, uri):
+        """
+        Resolve a remote URI. We only look locally.
+
+        uri -- the URI to resolve
+        """
+        name = uri.split('/')[-1]
+        local = os.path.join(_SCHEMA_DIR, 'messages', name)
+
+        if os.path.exists(local):
+            with open(local, 'rt') as f:
+                return json.load(f)
+        else:
+            print('Unable to find referenced schema:', name)
 
 
 class IpcClient:
@@ -27,6 +60,14 @@ class IpcClient:
         self.manager_socket = nnpy.Socket(nnpy.AF_SP, nnpy.REQ)
         self.manager_conn = \
             self.manager_socket.connect(_IPC_BASE + 'gateway.addonManager')
+
+        with open(os.path.join(_SCHEMA_DIR, 'schema.json'), 'rt') as f:
+            schema = json.load(f)
+
+        self.validator = jsonschema.Draft7Validator(
+            schema=schema,
+            resolver=Resolver()
+        )
 
         if verbose:
             print('IpcClient: Connected to server, registering...')
@@ -53,8 +94,10 @@ class IpcClient:
 
         try:
             resp = json.loads(resp.decode('utf-8'))
-            if not resp or 'messageType' not in resp or \
-                    resp['messageType'] != MessageType.PLUGIN_REGISTER_RESPONSE:  # noqa
+
+            self.validator.validate({'message': resp})
+
+            if resp['messageType'] != MessageType.PLUGIN_REGISTER_RESPONSE:
                 raise ValueError()
 
             self.plugin_socket = nnpy.Socket(nnpy.AF_SP, nnpy.PAIR)
@@ -67,3 +110,5 @@ class IpcClient:
         except ValueError:
             print('IpcClient: Unexpected registration reply from gateway: {}'
                   .format(resp))
+        except jsonschema.exceptions.ValidationError:
+            print('Invalid message received:', resp)
